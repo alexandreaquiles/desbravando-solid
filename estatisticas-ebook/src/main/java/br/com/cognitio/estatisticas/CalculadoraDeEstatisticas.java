85e68d1cc74285c3fe03a5a7bcd7a33d6cfe3ce3
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2022-03-19 16:10:04 -0300

    [LSP] Continuando a implementação das estatísticas

--- estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/CalculadoraDeEstatisticas.java
+++ estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/CalculadoraDeEstatisticas.java
@@ -7,6 +7,10 @@ import cotuba.plugin.Plugin;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.text.Normalizer;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class CalculadoraDeEstatisticas implements Plugin {
 
   @Override
@@ -17,6 +21,8 @@ public class CalculadoraDeEstatisticas implements Plugin {
   @Override
   public void aposGeracao(Ebook ebook) {
 
+    Map<String, Integer> contagemPalavras = new TreeMap<>();
+
     for (Capitulo capitulo : ebook.getCapitulos()) {
 
       String html = capitulo.getConteudoHTML();
@@ -25,12 +31,31 @@ public class CalculadoraDeEstatisticas implements Plugin {
 
       String textoDoCapitulo = doc.body().text();
 
-      String[] palavras = textoDoCapitulo.split("\\s+");
+      String textoDoCapituloSemPontuacao =
+          textoDoCapitulo.replaceAll("\\p{Punct}", " ");
+
+      String textoDoCapituloSemAcentos =
+          Normalizer.normalize(textoDoCapituloSemPontuacao, Normalizer.Form.NFD)
+              .replaceAll("[^\\p{ASCII}]", "");
+
+      String[] palavras = textoDoCapituloSemAcentos.split("\\s+");
 
       for (String palavra : palavras) {
-        System.out.println(palavra);
+        String emMaiusculas = palavra.toUpperCase();
+
+        contagemPalavras.put(emMaiusculas, 1);
+
       }
 
+    }
+
+    for (Map.Entry<String, Integer> contagem : contagemPalavras.entrySet()) {
+
+      String palavra = contagem.getKey();
+
+      Integer ocorrencias = contagem.getValue();
+
+      System.out.println(palavra + ": " + ocorrencias);
     }
 
   }
