f40967b2ecf0e011050089d104107371ebca2867
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2022-03-26 15:37:30 -0300

    [JPMS] wip: Explorando a falta de encapsulamento dos módulos

--- cotuba/cotuba-cli/src/main/java/cotuba/cli/Main.java
+++ cotuba/cotuba-cli/src/main/java/cotuba/cli/Main.java
@@ -1,12 +1,20 @@
 package cotuba.cli;
 
 import cotuba.application.Cotuba;
+import cotuba.domain.builder.CapituloBuilder;
+import cotuba.md.RenderizadorMDParaHTML;
 
 import java.nio.file.Path;
 
 public class Main {
 
   public static void main(String[] args) {
+
+    CapituloBuilder builder = new CapituloBuilder(); // opa!
+    System.out.println(builder);
+
+    RenderizadorMDParaHTML md = new RenderizadorMDParaHTML();  // eita!
+    System.out.println(md);
 
     Path arquivoDeSaida;
     boolean modoVerboso = false;
